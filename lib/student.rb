c88e4eb92c72c3af2b1df1debfaf9116a6441a00
Brandon Leeds <brandonleeds@gmail.com>
2018-01-17 00:15:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,6 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = (?)"
-    binding.pry
     new_from_db(DB[:conn].execute(sql, name).first)
   end
 end
