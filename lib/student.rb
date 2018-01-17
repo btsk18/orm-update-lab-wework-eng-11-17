040f9a53d84dae78c7d46adc51497e64bb132b9e
Brandon Leeds <brandonleeds@gmail.com>
2018-01-17 00:04:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,14 @@ class Student
   end
 
   def self.create_table
+    create = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(create)
   end
 
 end
